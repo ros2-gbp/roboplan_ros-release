1114c5f9cdf6d36e64ce79325b890e8ee99d06f8
Sebastian Castro <sebas.a.castro@gmail.com>
2026-08-21 21:09:31 -0400

    Revert "Trimmed the branch to only the roboplan_ros_examples/roboplan_ros_examples sub directory"
    This reverts commit 4527e7625e18e625df72d54effb5da56e47832c1.

---
+++ docs/source/conf.py
@@ -0,0 +1,113 @@
+import os
+import sys
+from pathlib import Path
+import subprocess
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+autoapi_dirs = [
+    "../../roboplan_ros_cpp/bindings/python",
+    "../../roboplan_ros_visualization/bindings/python",
+    "../../roboplan_ros_py/roboplan_ros_py",
+]
+
+for autoapi_dir in autoapi_dirs:
+    sys.path.insert(0, autoapi_dir)
+
+# -- Project information -----------------------------------------------------
+
+project = "roboplan-ros"
+copyright = "2025-2026, Open Planning"
+author = "Erik Holum"
+
+# The full version, including alpha/beta/rc tags
+version = release = "0.6.0"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "autoapi.extension",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.autosummary",
+    "sphinx_rtd_theme",
+    "sphinx_copybutton",
+    "breathe",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: list[str] = []
+
+# Mock imports for external dependencies.
+autodoc_mock_imports = [
+    "numpy",
+    "rclpy",
+    "roboplan",
+    "std_msgs",
+    "geometry_msgs",
+    "sensor_msgs",
+    "visualization_msgs",
+    "interactive_markers",
+    "tf2_ros",
+    "pinocchio",
+]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "private-members",
+]
+autoapi_type = "python"
+autoapi_template_dir = "_templates/autoapi"
+autoapi_add_toctree_entry = True
+autoapi_python_class_content = "both"
+autodoc_typehints = "description"
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+master_doc = "index"
+
+# -- Options for breathe -----------------------------------------------------
+breathe_default_project = "roboplan-ros"
+breathe_projects = {}
+breathe_projects_source = {}
+
+for package in [
+    "roboplan_ros_cpp",
+    "roboplan_ros_visualization",
+]:
+    # Generate Doxygen XML and add it to the breathe projects.
+    subprocess.call(f"cd ../../{package}/docs; rm -rf html/ xml/; doxygen", shell=True)
+    breathe_projects[package] = os.path.abspath(f"../../{package}/docs/xml")
+
+    # Add the package files to the breathe projects sources list.
+    package_path = Path(os.path.abspath(f"../../{package}/include/{package}"))
+    breathe_projects_source[package] = (
+        package_path.as_posix(),
+        [f for f in package_path.rglob("*.hpp")],
+    )
