0f81858f8afc44cc2639103bdede4fd768090c9f
Sebastian Castro <sebas.a.castro@gmail.com>
2026-08-21 21:08:35 -0400

    Revert "Trimmed the branch to only the roboplan_ros_visualization sub directory"
    This reverts commit 2460bf7df100327877713e79169eb74f107084ab.

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Source ROS and the RoboPlan workspace
+echo -e "Sourced ROS ${ROS_DISTRO}"
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+if [ -f /workspace/roboplan_ws/install/setup.bash ]
+then
+  echo "Sourced RoboPlan ROS workspace"
+  source /workspace/roboplan_ws/install/setup.bash
+fi
+
+# Execute the command passed into this entrypoint
+exec "$@"
